87a42492440f37e330cde9e0512ad87047b9b4de
chanhyeok3856 <grass3856@gmail.com>
2024-08-27 14:45:44 +0900

    0827

--- src/main/java/com/example/demo/dto/TodoDTO.java
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -7,7 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -19,5 +18,12 @@ public TodoDTO(final TodoEntity entity) {
 	this.id = entity.getId();
 	this.title = entity.getTitle();
 	this.done = entity.isDone();
+}
+public static TodoEntity toEntity(final TodoDTO dto) {
+	return TodoEntity.builder()
+			.id(dto.getId())
+			.title(dto.getTitle())
+			.done(dto.isDone())
+			.build();
 }
 }
--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,5 +1,11 @@
 package com.example.demo.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +15,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name="Todo")
 public class TodoEntity {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid")
 	private String id; // 이 오브젝트의 아이디
-	private String userid; // 이 오브젝트를 생성한 유저의 아이디
+	private String userId; // 이 오브젝트를 생성한 유저의 아이디
 	private String title; // Todo 타이틀 예) 운동 하기
 	private boolean done; // true - todo를 완료한 경우(checked)
 }
